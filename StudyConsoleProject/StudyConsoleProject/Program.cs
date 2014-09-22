539db2ee6d12f87998e165742d6b5f2ad32c57cd
mgz693 <megazone693@corp.auction.co.kr>
2014-09-22 15:12:40 +0900

    비교를 위한 Program.cs 업데이트

--- StudyConsoleProject/StudyConsoleProject/Program.cs
+++ StudyConsoleProject/StudyConsoleProject/Program.cs
@@ -9,10 +9,48 @@ namespace StudyConsoleProject
     {
         static void Main(string[] args)
         {
+            List<String> listOrigin = new List<string>();
+            listOrigin.Add("첫번째 아이템");
+            Console.WriteLine(listOrigin[0]);
+
+            listOrigin.Add("두번째 아이템");
+            ConsoleWriteAll(listOrigin);
+            
+            listOrigin.Remove("첫번째 아이템");
+            ConsoleWriteAll(listOrigin);
+
+            ConsoleWriteAllWithForeach(listOrigin);
+            
             YounList<String> list = new YounList<string>();
             list.Add("첫번째 아이템");
-            list.Add("두번째 아이템");
             Console.WriteLine(list[0]);
+
+            list.Add("두번째 아이템");
+            ConsoleWriteAll(list);
+
+            list.Remove("첫번째 아이템");
+            ConsoleWriteAll(list);
+
+            ConsoleWriteAllWithForeach(list);
+
+            Console.ReadLine();
+            
+        }
+
+        private static void ConsoleWriteAll(IList<string> targetList)
+        {
+            for (var i = 0; i < targetList.Count; ++i)
+            {
+                Console.WriteLine(targetList[i]);
+            }
+        }
+
+        private static void ConsoleWriteAllWithForeach(IEnumerable<string> targetList)
+        {
+            foreach (var item in targetList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
