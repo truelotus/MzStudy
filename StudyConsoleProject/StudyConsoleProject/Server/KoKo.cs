e7e9981adac5bfd7e6e5afb629466855b0897c32
mgz693 <megazone693@corp.auction.co.kr>
2014-10-02 17:39:54 +0900

    server 인터페이스 추가.

--- StudyConsoleProject/StudyConsoleProject/Server/KoKo.cs
+++ StudyConsoleProject/StudyConsoleProject/Server/KoKo.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
+using System.Web.Configuration;
+using System.ServiceModel.Web;
+using System.IO;
 
 namespace StudyConsoleProject.Server
 {
@@ -9,7 +13,29 @@ namespace StudyConsoleProject.Server
     {
         public string Test()
         {
+
+            Console.WriteLine(WebOperationContext.Current.IncomingRequest); // 웹요청이다!
+            Console.WriteLine(WebOperationContext.Current.OutgoingResponse); // 응답 - Client 한테 보내어진다.
             return "Let's Ko!";
+        }
+
+
+        public string Test2(string text)
+        {
+            return text;
+        }
+
+
+        public Stream Test3(string text)
+        {
+            WebOperationContext.Current.OutgoingResponse.ContentType = "text/html";
+            
+            var memStream = new MemoryStream();
+            var streamWriter = new StreamWriter(memStream);
+            streamWriter.WriteLine(text);
+            streamWriter.Flush();
+            memStream.Seek(0, SeekOrigin.Begin);
+            return memStream;            
         }
     }
 }
